f877386846fc33fc1dc75e2b15906d4004e94e82
Elienai <elienay.soares07@gmail.com>
2022-05-01 11:37:44 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/ListCategories';
+
+const categoriesRoutes = Router();
+
+categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
+
+categoriesRoutes.get('/', (request, response) => {
+  listCategoriesController.handle(request, response);
+});
+
+export { categoriesRoutes };
