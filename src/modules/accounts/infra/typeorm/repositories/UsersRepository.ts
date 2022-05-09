dbbdea6095ea443878968c2db99a0e68f040b67d
Elienai <elienay.soares07@gmail.com>
2022-05-08 21:43:00 -0300

    refactoring - reorganization of structurere

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -1,8 +1,9 @@
 import { getRepository, Repository } from 'typeorm';
 
-import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
-import { User } from '../../entities/User';
-import { IUsersRepository } from '../IUsersRepository';
+import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
+
+import { IUsersRepository } from '../../../repositories/IUsersRepository';
 
 class UserRepository implements IUsersRepository {
   private repository: Repository<User>;
