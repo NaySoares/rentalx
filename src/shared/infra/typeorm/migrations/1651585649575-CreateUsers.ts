62f18e2cd8fed6c53f79725c878b75801822bd8d
Elienai <elienay.soares07@gmail.com>
2022-11-04 10:44:48 -0300

    add migration rentals

--- src/shared/infra/typeorm/migrations/1651585649575-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1651585649575-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1651585649575 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
